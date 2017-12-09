303f31cb892e2cffc3732ff2e2432af0104713cb
Jonathan Guillotte-Blouin <jonathan.guillotte.blouin@gmail.com>
2017-12-09 18:15:28 -0500

    add testing functions

--- main_test.go
+++ main_test.go
@@ -6,6 +6,24 @@ import (
 	"./bhs"
 )
 
+type bhsAlgo func(r bhs.Ring) bhs.NodeID
+
+func runTest(algo bhsAlgo, t *testing.T) {
+	var size bhs.NodeID = 100
+
+	for i := bhs.NodeID(1); i < size; i++ {
+		r := bhs.BuildRing(i, uint64(size), false)
+
+		if result := algo(r); result != i {
+			t.Errorf("Expected %v, got %d", i, result)
+		}
+	}
+}
+
+func TestOptAvgTime(t *testing.T) {
+	runTest(optAvgTime, t)
+}
+
 func benchmarkOptAvgTime(i uint64, b *testing.B) {
 	r := bhs.BuildRing(bhs.NodeID(i-1), i, false)
 
@@ -14,6 +32,10 @@ func benchmarkOptAvgTime(i uint64, b *testing.B) {
 	}
 }
 func BenchmarkOptAvgTime10000(b *testing.B) { benchmarkOptAvgTime(1000, b) }
+
+func TestOptTime(t *testing.T) {
+	runTest(optTime, t)
+}
 
 func benchmarkOptTime(i uint64, b *testing.B) {
 	r := bhs.BuildRing(bhs.NodeID(i-1), i, false)
@@ -24,6 +46,10 @@ func benchmarkOptTime(i uint64, b *testing.B) {
 }
 func BenchmarkOptTime10000(b *testing.B) { benchmarkOptTime(1000, b) }
 
+func TestOptTeamSize(t *testing.T) {
+	runTest(optTeamSize, t)
+}
+
 func benchmarkOptTeamSize(i uint64, b *testing.B) {
 	r := bhs.BuildRing(bhs.NodeID(i-1), i, true)
 
@@ -33,6 +59,10 @@ func benchmarkOptTeamSize(i uint64, b *testing.B) {
 }
 func BenchmarkOptTeamSize10000(b *testing.B) { benchmarkOptTeamSize(1000, b) }
 
+func TestDivide(t *testing.T) {
+	runTest(divide, t)
+}
+
 func benchmarkDivide(i uint64, b *testing.B) {
 	r := bhs.BuildRing(bhs.NodeID(i-1), i, true)
 
@@ -41,6 +71,10 @@ func benchmarkDivide(i uint64, b *testing.B) {
 	}
 }
 func BenchmarkDivide10000(b *testing.B) { benchmarkDivide(1000, b) }
+
+func TestGroup(t *testing.T) {
+	runTest(group, t)
+}
 
 func benchmarkGroup(i uint64, b *testing.B) {
 	r := bhs.BuildRing(bhs.NodeID(i-1), i, true)
